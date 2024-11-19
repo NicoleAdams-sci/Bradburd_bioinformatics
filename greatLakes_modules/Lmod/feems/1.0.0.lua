3201e643ce63ab79e0db15af3a3215aa4f61db27
Nicole Adams <nicoleAdams.sci@gmail.com>
2024-11-19 17:23:08 -0500

    update feems lua

--- greatLakes_modules/Lmod/feems/1.0.0.lua
+++ greatLakes_modules/Lmod/feems/1.0.0.lua
@@ -2,7 +2,7 @@
 help([[
 Name: feems
 Version: 1.0.0
-Description: Fast Estimation of Effective Migration Surfaces (feems) is a python package implementing a statistical method for inferring and visualizing gene-flow in spatial population genetic data.
+Description: Fast Estimation of Effective Migration Surfaces (feems) is a Python package implementing a statistical method for inferring and visualizing gene-flow in spatial population genetic data.
 
 Initial One-Time Setup: 
 module load mamba
@@ -15,15 +15,40 @@ load("gcc")
 load("geos")
 load("mamba")
 
-
 -- Set the FEEMS environment variable
 local feems_dir = "/nfs/turbo/lsa-bradburd/shared/programs/feems"
 setenv("FEEMS", feems_dir)
 
--- Activate the feems Conda environment
--- Using the `execute{cmd = ...}` method to source the shell setup files and activate the environment within this module.
-execute{cmd = 'source ~/.bashrc && mamba activate ' .. feems_dir, modeA = {"load"}}
+-- Set the Conda environment paths
+local conda_bin = pathJoin(feems_dir, "bin")
+local conda_lib = pathJoin(feems_dir, "lib")
+local conda_python = pathJoin(feems_dir, "lib/python3.8/site-packages")  -- Update Python version if necessary
 
--- Optional: Add bin directory of the Conda environment to the PATH, if it's not automatically added
-prepend_path("PATH", pathJoin(feems_dir, "bin"))
+-- Add the bin, lib, and python paths of the Conda environment
+prepend_path("PATH", conda_bin)
+prepend_path("LD_LIBRARY_PATH", conda_lib)
+prepend_path("PYTHONPATH", conda_python)
 
+-- Ensure necessary Conda environment variables are set
+setenv("CONDA_PREFIX", feems_dir)
+setenv("CONDA_DEFAULT_ENV", feems_dir)
+setenv("CONDA_PYTHON_EXE", pathJoin(conda_bin, "python"))
+setenv("CONDA_EXE", pathJoin(conda_bin, "mamba"))
+
+-- Add logic for manually setting up the mamba environment
+execute{cmd = 'source ~/.bashrc', modeA = {"load"}}
+execute{cmd = 'mamba activate ' .. feems_dir, modeA = {"load"}}
+
+-- Set whatis and description
+whatis("Name: feems")
+whatis("Version: 1.0.0")
+whatis("Description: Fast Estimation of Effective Migration Surfaces (feems)")
+
+if mode() == "unload" then
+    -- Reset environment variables on module unload
+    unsetenv("FEEMS")
+    unsetenv("CONDA_PREFIX")
+    unsetenv("CONDA_DEFAULT_ENV")
+    unsetenv("CONDA_PYTHON_EXE")
+    unsetenv("CONDA_EXE")
+end
