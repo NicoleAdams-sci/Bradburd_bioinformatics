649a93ed00c7f355e8f7b55b928a3a0d69395e75
NicoleAdams-sci <nicoleAdams.sci@gmail.com>
2025-05-09 13:24:46 -0400

    update directory

--- greatLakes_modules/Lmod/SLiM/4.3.lua
+++ greatLakes_modules/Lmod/SLiM/4.3.lua
@@ -5,8 +5,11 @@ Version: 4.3
 Description: SLiM is an evolutionary simulation framework that combines a powerful engine for population genetic simulations with the capability of modeling arbitrarily complex evolutionary scenarios.
 ]])
 
+whatis("Name: SLiM")
+whatis("Version: 4.3")
+
 -- Define the base installation directory
-local base = "/nfs/turbo/lsa-bradburd/shared/programs/SLiM/"
+local base = "/nfs/turbo/lsa-bradburd/shared/programs/SLiM_v4.3"
 
 -- Set the PATH environment variable
 prepend_path("PATH", base)
