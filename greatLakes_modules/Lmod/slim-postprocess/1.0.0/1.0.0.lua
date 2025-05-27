4248022605eb0b75cedf02c4723587f9e4ec5e07
NicoleAdams-sci <nicoleAdams.sci@gmail.com>
2025-05-27 15:50:15 -0400

    initial commit

---
+++ greatLakes_modules/Lmod/slim-postprocess/1.0.0/1.0.0.lua
@@ -0,0 +1,21 @@
+-- -*- lua -*-
+help([[
+Post-processing environment for SLiM simulation outputs.
+Python: tskit, pyslim, msprime
+R: gaia (requires R module)
+]])
+
+whatis("Name: slim-postprocess")
+whatis("Version: 1.0.0")
+
+local base = "/nfs/turbo/lsa-bradburd/shared/programs/slim-postprocess_v1.0.0"
+
+-- Load R module dependency
+load("R/4.4.0")  -- or appropriate version
+
+-- Set up Python environment
+prepend_path("PATH", pathJoin(base, "env/bin"))
+setenv("SLIM_POSTPROCESS_ROOT", base)
+
+-- Optional: Set R library path if you install gaia to module directory
+-- prepend_path("R_LIBS_USER", pathJoin(base, "R_libs"))
