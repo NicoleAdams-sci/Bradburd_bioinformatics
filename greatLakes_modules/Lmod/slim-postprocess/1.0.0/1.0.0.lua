3feda06a4f5c460a5512cab5148c0de62d27d2dc
NicoleAdams-sci <nicoleAdams.sci@gmail.com>
2025-05-27 16:00:44 -0400

    added version messages on loading

--- greatLakes_modules/Lmod/slim-postprocess/1.0.0/1.0.0.lua
+++ greatLakes_modules/Lmod/slim-postprocess/1.0.0/1.0.0.lua
@@ -19,3 +19,11 @@ setenv("SLIM_POSTPROCESS_ROOT", base)
 
 -- Optional: Set R library path if you install gaia to module directory
 -- prepend_path("R_LIBS_USER", pathJoin(base, "R_libs"))
+
+-- Display version info when loading
+if (mode() == "load") then
+    LmodMessage("Loading slim-postprocess v1.0.0")
+    LmodMessage("  Python packages: tskit v0.6.4, pyslim v1.0.4, msprime v1.3.4")
+    LmodMessage("  R packages: gaia")
+    LmodMessage("  R version: 4.4.0")
+end
